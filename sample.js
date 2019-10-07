1898d0d95005c7dce261ad970a33087898b96f50
reireynoso <reireynoso@gmail.com>
2019-10-07 15:40:50 -0400

    js basics

---
+++ sample.js
@@ -0,0 +1,19 @@
+function sayHello(){
+    console.log('Hello')
+}
+
+sayHello()
+
+//function declation 
+
+function addTheStuff(){
+
+}
+
+//function expression 
+
+const addTheStuff = function(){
+
+}
+
+
